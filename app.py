c9cd29ce9de1be64df04c620e9059286e33965d0
Prasann Agarwal <prasann.agarwal@gmail.com>
2020-10-28 21:09:37 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import pred
-from flask import Flask, request
-from flask_restful import Resource, Api
+import flask
+import flask_restful
+# from flask import Flask, request
+# from flask_restful import Resource, Api
 import math
 import json
 import pandas as pd
@@ -21,14 +23,26 @@ from warnings import catch_warnings
 from warnings import filterwarnings
 from sklearn.metrics import mean_squared_error
 
-app = Flask(__name__)
-api = Api(app)
+app = flask.Flask(__name__, template_folder='templates')
+# api = Api(app)
 
-class fc(Resource):
-    def get(self, first_number, second_number):
-        return {pred.sales_forecast(first_number,second_number)}
+@app.route('/', methods=['GET','POST'])
+def main():
+	if (flask.request.method == 'GET'):
+		return (flask.render_template('main.html'))
+    if (flask.request.method == 'POST'):
+    	item_id=flask.request.form['item_id']
+		firm_id=flask.request.form['firm_id']
+		res=pred.sales_forecast(item_id,firm_id)
+		return (flask.render_template('main.html',result=res))
+
+# class fc(Resource):
+#     def get(self, first_number, second_number):
+#         return {'data' :pred.sales_forecast(first_number,second_number)}
     
-api.add_resource(fc, '/4cast/<first_number>/<second_number>')
+# api.add_resource(fc, '/4cast/<first_number>/<second_number>')
+
+
 
 if __name__=='__main__':
     app.run()
